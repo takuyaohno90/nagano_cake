2407ff659b014f01df3feb20c25f2280a5bd3364
takuyaohno90 <131519948+takuyaohno90@users.noreply.github.com>
2023-06-15 16:22:48 +0000

    管理者商品途中

---
+++ app/controllers/admin/items_controller.rb
@@ -0,0 +1,32 @@
+class Admin::ItemsController < ApplicationController
+  def index
+
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    item = Item.new(item_params)
+    item.save
+    redirect_to admin_items_path
+  end
+
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :image, :introduction, :price, :is_active)
+  end
+
+end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,9 @@
 class Item < ApplicationRecord
+  belongs_to :genre
+  has_one_attached :image
+  validates :name, presence: true
+  validates :introduction, presence: true
+  validates :price, presence: true
+  validates :is_active, presence: true
+  validates :image, presence: true
 end
