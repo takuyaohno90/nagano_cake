a2fc6c10506576b007e0b574346e7f52f3f77391
takuyaohno90 <131519948+takuyaohno90@users.noreply.github.com>
2023-06-25 16:09:02 +0000

    注文処理顧客側終了

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -5,33 +5,65 @@ class Public::OrdersController < ApplicationController
 
   def check
     @order = Order.new(order_check_params)
+    @order.customer_id = current_customer.id
+    @order.postage = 800
     if params[:order][:select_button] == "1" then
       @order.post_code = current_customer.postal_code
       @order.shipping_address = current_customer.address
       @order.name = current_customer.last_name + current_customer.first_name
-    byebug
-      @order.save
     elsif params[:order][:select_button] == "2" then
-      address = current_customer.addresses.find_by(id: params[:address_id])
+      address = current_customer.addresses.find_by(id: params[:order][:address_id])
+      @order.post_code = address.postal_code
+      @order.shipping_address = address.address
+      @order.name = address.name
+    end
+    @cart_items= current_customer.cart_items.all
+    render 'check'
+  end
+
+  def create
+    @order = Order.new(order_params)
+    cart_items = current_customer.cart_items.all
+    if @order.save
+      cart_items.each do |cart_item|
+        order_item = OrderItem.new
+        order_item.order_id = @order.id
+        order_item.item_id = cart_item.item_id
+        order_item.buy_price = cart_item.item.price
+        order_item.amount = cart_item.amount
+        order_item.save
+      end
+      cart_items.destroy_all
+      redirect_to finish_orders_path
+    else
+      @order = Order.new(order_params)
+      render :check
+
     end
   end
 
   def finish
   end
 
-  def create
-  end
 
   def index
+    @order = current_customer.orders.all
   end
 
   def show
+    order_id = params[:id]
+    @order = Order.find(params[:id])
+    @order_item = OrderItem.where(order_id: order_id)
   end
 
   private
 
   def order_check_params
     params.require(:order).permit(:payment, :post_code, :shipping_address, :name)
+  end
+
+  def order_params
+    params.require(:order).permit(:customer_id, :payment, :post_code, :shipping_address, :name, :invoice, :postage)
   end
 
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,22 @@
 class Order < ApplicationRecord
   enum payment_method: {credit_card: 0, transfer: 1}
+  enum order_status: {stay: 0, check: 1, making: 2, standby: 3, finish: 4}
   belongs_to :customer
   has_many :order_items
+
+  validates :customer_id, presence: true
+  validates :post_code, presence: true
+  validates :shipping_address, presence: true
+  validates :name, presence: true
+  validates :postage, presence: true
+  validates :invoice, presence: true
+  validates :payment, presence: true
+  validates :order_status, presence: true
+
+  def confirm_one
+  end
+
+  def confirm_two
+
+  end
 end
