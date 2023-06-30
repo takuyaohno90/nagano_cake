de943145693ffdfe453fabdee95ea29c5cf181d0
takuyaohno90 <131519948+takuyaohno90@users.noreply.github.com>
2023-06-30 15:23:01 +0000

    ステータス更新完了

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -1,5 +1,28 @@
 class Admin::OrderDetailsController < ApplicationController
   def update
-    byebug
+    completed = 0
+    @order_item = OrderItem.find(params[:id])
+    @order_item.update(order_item_params)
+    order_id = @order_item.order_id
+    order_items = OrderItem.where(order_id: order_id)
+    order_items.each do |order_item|
+      unless order_item.production_status == "finish"
+        completed = true
+        break
+      end
+    end
+
+    if completed == true
+    else
+      @order_item.order.update(order_status: "standby")
+    end
+    redirect_to admin_order_path(@order_item.order_id)
+
+  end
+
+  private
+
+  def order_item_params
+    params.require(:order_item).permit(:production_status)
   end
 end
--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -4,13 +4,21 @@ class Admin::OrdersController < ApplicationController
   end
 
   def update
-    byebug
-    order = Order.find(params[:id])
+    @order = Order.find(params[:id])
+    @order_item = @order.order_items
+    if params[:order][:order_status] == "check"
+      @order_item.each do |order_item|
+        order_item.update(production_status: "stanby")
+      end
+      @order.update(order_params)
+      redirect_to admin_order_path(@order.id)
+    end
+
   end
 
   private
 
-  def order_end
+  def order_params
     params.require(:order).permit(:order_status, :production_status)
   end
 
